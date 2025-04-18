b13aac9a2d3ca094cc0c9e14e48d68fd93ef2a07
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-04-18 17:48:07 -0500

    Fix variables

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,8 +10,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  tenant_id       = var.ARM_TENANT_ID
 }
