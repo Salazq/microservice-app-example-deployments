9de18db326ba88faa252931e70ba79cea51f0be4
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-04-18 18:42:27 -0500

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "resource_group_location" {
   default     = "East US" # Cambiar a una región más económica
   description = "Location of the resource group."
 }
+
+variable "skip_aad_permissions" {
+  description = "Skip Azure AD permissions check"
+  type        = bool
+  default     = false
+}
