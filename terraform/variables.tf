fe7d2f1bb49105b46e6991d24694ae30ade42a51
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-04-18 17:10:46 -0500

    Fix variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,24 @@ variable "resource_group_location" {
   default     = "East US" # Cambiar a una región más económica
   description = "Location of the resource group."
 }
+
+variable "ARM_CLIENT_ID" {
+  description = "The client ID of the Azure service principal"
+  type        = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = "The client secret of the Azure service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "The subscription ID for the Azure account"
+  type        = string
+}
+
+variable "ARM_TENANT_ID" {
+  description = "The tenant ID for the Azure account"
+  type        = string
+}
