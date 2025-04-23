6b005b869991e5c487db15bca18234a3ecd6775b
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-04-22 19:18:52 -0500

    Fix comments

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_location" {
-  default     = "East US" # Cambiar a una región más económica
+  default     = "East US" 
   description = "Location of the resource group."
 }
 
@@ -12,5 +12,5 @@ variable "disable_aad_api" {
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
-  default     = "bfc3d097-7e73-40b8-aa34-e70b307c0867" # Puedes poner un valor por defecto o dejarlo vacío
+  default     = "bfc3d097-7e73-40b8-aa34-e70b307c0867" 
 }
